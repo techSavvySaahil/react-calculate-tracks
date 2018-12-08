3198e16a0acd075b0ff44f0566254c7910dcebf2
techSavvySaahil <saahil.madaan@gmail.com>
2018-12-08 20:55:40 +0530

    Updates

---
+++ precache-manifest.44303335519eb4fdb674a974ae2f580c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ca0c2a2284d787457f6b7e7a147659",
+    "url": "/react-calculate-tracks/static/media/loco.19ca0c2a.jpg"
+  },
+  {
+    "revision": "100e52f4c1124f6afa895086cebff5c8",
+    "url": "/react-calculate-tracks/static/media/carriage.100e52f4.jpg"
+  },
+  {
+    "revision": "d55d03a7e3bc01347afd",
+    "url": "/react-calculate-tracks/static/js/runtime~main.d55d03a7.js"
+  },
+  {
+    "revision": "896a9161a366f451b7b4",
+    "url": "/react-calculate-tracks/static/js/main.896a9161.chunk.js"
+  },
+  {
+    "revision": "479387793fc5d7ae8ab5",
+    "url": "/react-calculate-tracks/static/js/1.47938779.chunk.js"
+  },
+  {
+    "revision": "896a9161a366f451b7b4",
+    "url": "/react-calculate-tracks/static/css/main.b888e896.chunk.css"
+  },
+  {
+    "revision": "5726b040c640a506cfda0db38681a107",
+    "url": "/react-calculate-tracks/index.html"
+  }
+];
